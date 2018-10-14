ad91d561178322406396044876f00f29a61b8e14
Ian Danforth <iandanforth@gmail.com>
2018-10-14 11:05:35 -0700

    Remove old cruft, add new cruft

--- main.py
+++ main.py
@@ -1,51 +1,26 @@
-import pickle
-import os
 import time
+import csv
 
-from lib import crawl, db, api
+from lib import db, api
 
-# RATE LIMITING - 200 HITS PER MINUTE ACCORDING TO TIM
-
-text_db_filename = "db.p"
-
-# def get_company_text(company):
-def save_db(text_db):
-    with open(text_db_filename, "wb") as f:
-        pickle.dump(text_db, f)
-def load_db():
-    return pickle.load(open(text_db_filename, "rb"))
-
-# Load text db
-if not os.path.isfile(text_db_filename):
-    save_db({})
-text_db = pickle.load(open(text_db_filename, "rb"))
-
-def init_text_db():
-    companies = db.valid_companies()
-    company = companies[0]
-    company_url = company["url"]
-    company_name = company["name"]
-    if company_name not in text_db:
-        url_to_text = crawl.website_plaintext(company_url)
-        text_db[company_name] = url_to_text
-        save_db(text_db)
-    else:
-        url_to_text = text_db[company_name]
-    text = " ".join(list(url_to_text.values()))
 
 def print_dict(d):
     print("\n".join(["%s: %s" % (k, v) for (k, v) in sorted(d.items())]))
 
-def download_funding_info():
-    conn, curr, err = db.open_connection()
-    companies = db.select_companies()
 
-    company_funding_info = db.select_companies_with_funding()
+def download_funding_info(conn, curr):
+    companies = db.select_companies(conn, curr)
+
+    company_funding_info = db.select_companies_with_funding(conn, curr)
+    print("Filtering out existing companies ...")
     companies = [c for c in companies if c["company_uuid"] not in company_funding_info]
+    print("Done.")
+    print(len(companies))
     for company in companies:
         permalink = company["company_permalink"]
         company_uuid = company["company_uuid"]
         try:
+            print("Fetching data for: ", permalink)
             funding_rounds = api.fetch_company_funding_details(permalink)
             for round_ in funding_rounds:
                 db.insert_funding(conn, curr, company_uuid, round_)
@@ -53,17 +28,15 @@ def download_funding_info():
             print("ERROR WITH COMPANY %s" % company_uuid)
             print(e)
             print("------")
-            curr.close()
-            conn.close()
-            conn, curr, _ = db.open_connection()
             time.sleep(10)
+
 
 def print_locations():
     locations = api.fetch_locations()
     cities = list(sorted(list(locations["city"])))
     countries = list(sorted(list(locations["country"])))
     continents = list(sorted(list(locations["continent"])))
-     
+
     print("Cities")
     print("\n".join(cities))
     print("---")
@@ -74,15 +47,31 @@ def print_locations():
     print("\n".join(continents))
     print("---")
 
-if __name__ == "__main__":
-    pass
-    # conn, curr, err = db.open_connection()
-    # db.injest_ai_companies(conn, curr)
-    # category_dict = db.get_category_uuids(db.ai_category_to_uuid)
-    # for name, uuid in sorted(category_dict.items()):
-    #     print("Injesting '%s' companies" % name)
-    #     db.injest_companies_for_category(conn, curr, uuid)
 
-    # db.injest_filtered_companies(conn, curr)
-    # db.injest_companies(conn, curr)
-    # db.injest_categories(conn, curr)
+if __name__ == "__main__":
+    conn, curr, err = db.open_connection(dbname='crunchy')
+    if err: 
+        quit()
+
+    # Populate the CATEGORIES table
+    db.injest_categories(conn, curr)
+    # Request data on all companies which fall into the AI
+    # categories defined in db.py
+    db.injest_ai_companies(conn, curr)
+
+    # Use that list of companies to get funding info
+    print("Downloading funding data. This will take 1+ hours.")
+    download_funding_info(conn, curr)
+    print("Done.")
+
+    # Use that data to calculate funding by year
+    csv_file = 'funding_per_year_by_country.csv'
+    with open(csv_file, 'w') as fh:
+        writer = csv.writer(fh)
+        writer.writerow(db.countries)
+        for year in range(1992, 2019):
+            year_row = []
+            for country in db.countries:
+                year_total = db.select_funding_by_year(conn, curr, year, country=country)
+                year_row.append(year_total)
+            writer.writerow(year_row)
